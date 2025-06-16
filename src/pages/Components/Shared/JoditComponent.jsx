0f59dcd00b162c57301e000e66e19d8401956f36
hosain ali <hosainahmed534745@gmail.com>
2025-06-16 11:51:24 +0600

    update jodit editor config

--- src/pages/Components/Shared/JoditComponent.jsx
+++ src/pages/Components/Shared/JoditComponent.jsx
@@ -1,20 +1,55 @@
-import React, { memo, useRef } from 'react';
+import React, { memo, useMemo, useRef } from 'react';
 import JoditEditor from 'jodit-react';
 
 const JoditComponent = ({ content, setContent }) => {
   const editor = useRef(null);
-
+  const config = useMemo(
+    () => ({
+      readonly: false,
+      toolbarSticky: false,
+      minHeight: 500,
+      buttons: [
+        'bold',
+        'italic',
+        'underline',
+        'strikethrough',
+        'ul',
+        'ol',
+        'outdent',
+        'indent',
+        'font',
+        'fontsize',
+        'brush',
+        'paragraph',
+        'image',
+        'table',
+        'link',
+        'align',
+        'undo',
+        'redo',
+        'hr',
+        'eraser',
+        'copyformat',
+        'fullsize',
+        'preview',
+      ],
+      uploader: {
+        insertImageAsBase64URI: true,
+      },
+      style: {
+        fontFamily: 'inherit',
+        fontSize: '16px',
+      },
+    }),
+    []
+  );
   return (
     <div>
       <JoditEditor
         ref={editor}
         value={content || ''}
         onBlur={(newContent) => setContent(newContent)}
-        config={{
-          readonly: false,
-          toolbarSticky: false,
-          minHeight: 600,
-        }}
+        config={config}
       />
     </div>
   );
