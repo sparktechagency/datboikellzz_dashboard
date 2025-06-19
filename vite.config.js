ca25b301407d4891a64775bc4117697c55325cf9
hosain ali <hosainahmed534745@gmail.com>
2025-06-19 15:45:41 +0600

    s

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   server: {
-    // host: '0.0.0.0',
-    port: '8003',
+    host: '0.0.0.0',
+    port: 8003,
   },
   preview: {
-    // host: '0.0.0.0',
-    port: '8003',
+    host: '0.0.0.0',
+    port: 8003,
   },
   plugins: [tailwindcss()],
 });
