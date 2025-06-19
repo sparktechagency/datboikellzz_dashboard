ecd2450bbe7cb1a9f56b3f516cba00a3a9b19ab8
ThakurSaad <thakursaad613@gmail.com>
2025-06-19 16:00:07 +0600

    git

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 8003,
   },
-  preview: {
-    host: '0.0.0.0',
-    port: 8003,
-  },
+  // preview: {
+  //   host: '0.0.0.0',
+  //   port: 8003,
+  // },
   plugins: [tailwindcss()],
 });
