566f9ee97da82a0baa8bf60fd370efe6b0f94117
hosain ali <hosainahmed534745@gmail.com>
2025-06-19 15:43:04 +0600

    vite update

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    // host: '0.0.0.0',
     port: '8003',
   },
   preview: {
-    host: '0.0.0.0',
+    // host: '0.0.0.0',
     port: '8003',
   },
   plugins: [tailwindcss()],
