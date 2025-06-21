541a11298dfcd4b0c9e33fa4dfad5a25e1c4d2d5
ThakurSaad <thakursaad613@gmail.com>
2025-06-21 18:14:53 +0600

    base url changed

--- src/Utils/server.js
+++ src/Utils/server.js
@@ -1,13 +1,12 @@
 // export const url = import.meta.env.VITE_API_URL;
-export const url = 'http://63.178.140.14:8001';
+export const url = "http://3.76.70.78:8001";
 
 export const imageUrl = (image) => {
   return image
-    ? image?.startsWith('http')
+    ? image?.startsWith("http")
       ? image
-      : image?.startsWith('/')
+      : image?.startsWith("/")
       ? `${url}${image}`
       : `${url}/${image}`
-    : 'https://placehold.co/400';
+    : "https://placehold.co/400";
 };
-
