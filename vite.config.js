725b17b9366b415c095303839d7e1337bf71de95
ThakurSaad <thakursaad613@gmail.com>
2025-06-23 12:51:19 +0600

    preveiw

--- vite.config.js
+++ vite.config.js
@@ -21,11 +21,12 @@ export default defineConfig({
     // port: 8003,
     port: 8000,
   },
-  // preview: {
-  //   host: "0.0.0.0",
-  //   // port: 8003,
-  //   port: 8000,
-  // },
+  preview: {
+    // host: "0.0.0.0",
+    // port: 8003,
+    // port: 8000,
+    allowedHosts: ["admin.betwisepicks.com"],
+  },
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
